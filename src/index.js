7fa8a58e78b68af5cc05dfcf0c1ce238e4cfc775
David Macharia <macharia3041@gmail.com>
2020-04-11 18:16:03 +0300

    challenge-04

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
-import estimatorHelper from '../src/estimatorHelper';
+import estimatorHelper from './estimatorHelper';
 
 let data = {
-    region: {}
-}
+	region: {}
+};
 
-$('#myform').submit(function() {
-    // get all the inputs into an array.
-    var $inputs = $('#myform :input');
+$('#myform').submit(() => {
+	const $inputs = $('#myform :input');
 
-    // not sure if you wanted this, but I thought I'd add it.
-    // get an associative array of just the values.
-    var values = {};
-    $inputs.each(function() {
-        if ($(this).val()) {
-        	values[this.name] = $(this).val();
-        }
-    });
-    delete values.register;
-    
-    data = {
-        ...data,
-        ...values
-    }
-    estimatorHelper(data)
+	const values = {};
+	$inputs.each(function () {
+		if ($(this).val()) {
+			values[this.name] = $(this).val();
+		}
+	});
+	delete values.register;
+
+	data = {
+		...data,
+		...values
+	};
+	estimatorHelper(data);
 });
