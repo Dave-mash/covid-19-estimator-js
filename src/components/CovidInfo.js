aedd37ff3ba14fb546f61a47a443f6661edf0af7
David Macharia <macharia3041@gmail.com>
2020-04-13 17:05:13 +0300

    challenge-04

--- src/components/CovidInfo.js
+++ src/components/CovidInfo.js
@@ -4,8 +4,8 @@ const CovidInfo = () => (
 	<Fragment>
 		<div className="form-left">
 			<h2>COVID-19 INFORMATION</h2>
-			<p className="text-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et molestie feugiat sed. Diam volutpat commodo.</p>
-			<p className="text-2"><span>Eu ultrices:</span> Vitae auctor eu augue ut. Malesuada nunc vel risus commodo viverra. Praesent elementum facilisis leo vel.</p>
+			<b className="text-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et molestie feugiat sed. Diam volutpat commodo.</b>
+			<b className="text-2"><span>Eu ultrices:</span> Vitae auctor eu augue ut. Malesuada nunc vel risus commodo viverra. Praesent elementum facilisis leo vel.</b>
 		</div>
 	</Fragment>
 );
