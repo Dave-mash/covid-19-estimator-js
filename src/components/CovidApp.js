7e4172ae205940cf47dbcee53f1b4e9c3e0e015f
David Macharia <macharia3041@gmail.com>
2020-04-14 22:08:46 +0300

    challenge-04

--- src/components/CovidApp.js
+++ src/components/CovidApp.js
@@ -5,16 +5,14 @@ const CovidInfo = React.lazy(() => import('./CovidInfo'));
 
 
 const CovidApp = () => (
-	<Fragment>
-		<Suspense fallback={<div>Loading...</div>}>
-			<div className="page-content">
-				<div className="form-v4-content">
-					<CovidInfo />
-					<CovidForm />
-				</div>
+	<Suspense fallback={<div>Loading...</div>}>
+		<div className="page-content">
+			<div className="form-v4-content">
+				<CovidInfo />
+				<CovidForm />
 			</div>
-		</Suspense>
-	</Fragment>
+		</div>
+	</Suspense>
 );
 
 export default CovidApp;
--- src/components/CovidInfo.js
+++ src/components/CovidInfo.js
@@ -1,13 +1,11 @@
 import React, { Fragment } from 'react';
 
 const CovidInfo = () => (
-	<Fragment>
-		<div className="form-left">
-			<h2>COVID-19 INFORMATION</h2>
-			<b className="text-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et molestie feugiat sed. Diam volutpat commodo.</b>
-			<b className="text-2"><span>Eu ultrices:</span> Vitae auctor eu augue ut. Malesuada nunc vel risus commodo viverra. Praesent elementum facilisis leo vel.</b>
-		</div>
-	</Fragment>
+	<div className="form-left">
+		<h2>COVID-19 INFORMATION</h2>
+		<b className="text-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et molestie feugiat sed. Diam volutpat commodo.</b>
+		<b className="text-2"><span>Eu ultrices:</span> Vitae auctor eu augue ut. Malesuada nunc vel risus commodo viverra. Praesent elementum facilisis leo vel.</b>
+	</div>
 );
 
 export default CovidInfo;
